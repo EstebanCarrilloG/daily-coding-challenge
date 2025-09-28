a937ced77d4989f0a5785f2c1a53e014ae9a2a03
EstebanCarrilloG <estexav0996@gmail.com>
2025-09-28 14:15:09 -0500

    create file: challenge 18-08-2025.

---
+++ challenges/august-2025/18.js
@@ -0,0 +1,18 @@
+//Factorializer
+
+/**
+ * Returns the factorial of a given number.
+ * The factorial of a number is the product of all positive integers less than or equal to that number.
+ * @param {number} n - The number to calculate the factorial of.
+ * @returns {number} The factorial of the given number.
+ */
+export function factorial(n) {
+  if (n == 0) n = 1;
+  let j = n;
+  for (let i = n; i > 1; i--) {
+    j *= i - 1;
+  }
+
+  return j;
+}
+console.log(factorial(1));
