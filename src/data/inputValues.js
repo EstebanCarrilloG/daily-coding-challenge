37c9fc0d9d368a098f495f517c4beaa2a473027a
EstebanCarrilloG <estexav0996@gmail.com>
2025-09-30 14:14:09 -0500

    create inputValues to store input values for challenges functions.

---
+++ src/data/inputValues.js
@@ -0,0 +1,26 @@
+// August 2025
+
+export const vowelBalance = "123A#b!E&*456-o.U";
+export const fibonacciSequence = { startSequence: [21, 32], length: 10 };
+export const spaceJam = "   free   Code   Camp   ";
+export const jbelmu = "hello world";
+export const areAnagrams = { word1: "listen", word2: "silent" };
+export const findTarget = { arr: [1, 2, 3, 4, 5], target: 9 };
+export const factorial = 5;
+export const sumOfSquares = 5;
+export const squaresWithThree = 100;
+export const milePace = { minutes: 2, time: "24:31" };
+export const decode = "Xlmw mw e wigvix qiwweki.";
+
+// September 2025
+
+export const allUnique = "hola";
+export const costToFill = { gallons: 20, cost: 0, price: 4.0 };
+export const numberOfPhotos = { cost: 3.5, size: 750 };
+export const isPerfectSquare = 9;
+export const secondLargest = [1, 2, 3, 4, 4, 4];
+export const speeding = [58, 50, 60, 55];
+export const isSpam = "+0 (200) 111-1182";
+export const getHeadings = "username , email , signup date ";
+export const getLongestWord = "This sentence has multiple long words.";
+export const formatNumber = "05552340182";
